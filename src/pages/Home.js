c88b9ec653d29c9e9d50b8fbda1065a9af2e53e0
SanaProjects <sanaswastika21@gmail.com>
2022-06-13 15:14:46 +0530

    fetch API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,41 @@
 import React, {useState} from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
-    const onSearch = () => {
-      
-      fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result =>{
-        console.log(result);
-      })
-    }
+    
     const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
+
     const onInputChange = (ev) => {
-      setInput(ev.target.value);
-      
+      setInput(ev.target.value);      
+    };
+
+    const onSearch = () => {
+      apiGet(`search/shows?q=${input}`).then(result =>{
+        setResults(result);
+      // fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result =>{
+      //   setResults(result);
+      //   console.log(result);
+      });
     };
 
     const onKeyDown = (ev) => {
        if(ev.keyCode === 13){
-        onSearch()
+        onSearch();
        }
+    };
+
+    const renderResults = () => {
+       if(results && results.length === 0) {
+        return <div>No Results</div>
+       }
+
+       if(results && results.length > 0) {
+        return <div> {results.map( (item)=><div key={item.show.id}>{item.show.name}</div>)} </div>
+       }
+
+       return null;
     }
 
 
@@ -26,6 +44,7 @@ const Home = () => {
         <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
          <button type='button' onClick={onSearch}>Search</button>
          {/* This is hooome page... */}
+         {renderResults()}
     </MainPageLayout>
   )
 }
